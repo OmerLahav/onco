8e85155e1bf016595a8ed79bcdb27889473ce9f3
Tomerch991 <33205633+Tomerch991@users.noreply.github.com>
2018-04-01 21:11:05 +0300

    upd

--- front/js/medications.js
+++ front/js/medications.js
@@ -1,10 +1,10 @@
 var d = new Date();
 
-function next(){
+function next()
+{
 var nextDay=new Date(d);
 var dateValue = nextDay.getDate() + 1;
 nextDay.setDate(dateValue);
-return nextDay;
 document.getElementById("current").innerHTML = nextDay.toLocaleString();
 
 }
@@ -13,12 +13,13 @@ function prev(){
 var prevDay=new Date(d);
 var dateValue = prevDay.getDate() - 1;
 prevDay.setDate(dateValue);
-return prevDay;
+
 document.getElementById("current").innerHTML = prevDay.toLocaleString();
 
 }
 
 document.getElementById("current").onload = curr();
+
 
 function curr() {
     document.getElementById("current").innerHTML = d.toLocaleString();
