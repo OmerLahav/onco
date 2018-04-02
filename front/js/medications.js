440989a2575cd4308c68d288394633818c555610
Tomerch991 <33205633+Tomerch991@users.noreply.github.com>
2018-04-02 21:24:10 +0300

    UPD

--- front/js/medications.js
+++ front/js/medications.js
@@ -5,18 +5,60 @@ function timeChecker() {
     alert(n);
      if(n>=6 && n<=12)
      {
-     document.getElementById("card-morrning").style.backgroundColor = "#e6e600"; 
+     document.getElementById("card-morrning").style.backgroundColor = "rgb(0,200,200)"; 
+        document.getElementById("afternoon-akamol-one").disabled = true;
+         document.getElementById("afternoon-akamol-two").disabled = true;
+         document.getElementById("evening-akamol-one").disabled = true;
+         document.getElementById("evening-akamol-two").disabled = true;
+         document.getElementById("night-akamol-one").disabled = true;
+         document.getElementById("night-akamol-two").disabled = true;
+         $(".afternoon-box, .night-box, .evening-box").click(function() {
+              alert("Now it is morning time");
+            });
+         
     }
     else if (n>12 && n<=18)
     {
-      document.getElementById("card-afternoon").style.backgroundColor = "#ff9900"; 
+      document.getElementById("card-afternoon").style.backgroundColor = "rgb(0,200,200)"; 
+        document.getElementById("morning-akamol-one").disabled = true;
+         document.getElementById("morning-akamol-two").disabled = true;
+         document.getElementById("evening-akamol-one").disabled = true;
+         document.getElementById("evening-akamol-two").disabled = true;
+         document.getElementById("night-akamol-one").disabled = true;
+         document.getElementById("night-akamol-two").disabled = true;
+        $(".morning-box, .night-box, .evening-box").click(function() {
+              alert("Now it is afternoon time");
+            });
     }
     else if (n>18 && n<=24)
     {
-      document.getElementById("card-evening").style.backgroundColor = "#4747d1"; 
+      document.getElementById("card-evening").style.backgroundColor = "rgb(0,200,200)"; 
+       document.getElementById("morning-akamol-one").disabled = true;
+         document.getElementById("morning-akamol-two").disabled = true;
+         document.getElementById("afternoon-akamol-one").disabled = true;
+         document.getElementById("afternoon-akamol-two").disabled = true;
+         document.getElementById("night-akamol-one").disabled = true;
+         document.getElementById("night-akamol-two").disabled = true;
+        $(".morning-box, .night-box, .afternoon-box").click(function() {
+              alert("Now it is evening time");
+            });
+
     }
+    
     else
     {
-          document.getElementById("card-morrning").style.backgroundColor = "#3d3d5c"; 
+          document.getElementById("card-morrning").style.backgroundColor = "rgb(0,200,200)"; 
+        document.getElementById("morning-akamol-one").disabled = true;
+         document.getElementById("morning-akamol-two").disabled = true;
+         document.getElementById("afternoon-akamol-one").disabled = true;
+         document.getElementById("afternoon-akamol-two").disabled = true;
+         document.getElementById("evening-akamol-one").disabled = true;
+         document.getElementById("evening-akamol-two").disabled = true;
+        $(".afternoon-box, .night-box, .morning-box").click(function() {
+              alert("Now it is night time");
+            });
     }
 }
+
+ 
+
