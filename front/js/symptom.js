4c1dc9a719a70e98c556f5997704ff864e653fe9
Tomerch991 <33205633+Tomerch991@users.noreply.github.com>
2018-04-08 02:46:48 +0300

    UPD

--- front/js/symptom.js
+++ front/js/symptom.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
             position++;
         }
     });
-    
+
     $("#back").click(function () {
         if (position == 1) {
             $("#page-one").show();
@@ -30,11 +30,15 @@ $(document).ready(function () {
         }
     });
 
+    //-----JS for report symptom one-------------
+
+
     $(".symp-one").click(function () {
-        $(".report-levels").show();
+                $(".section-symp-2,.section-symp-3").hide();
+        $(".section-symp-1").show();
     });
 
-   //check if user chaked level
+    //check if user chaked level
     $('#save-symp-one').click(function () {
         if (!$("input[name='symptom-1']:checked").val()) {
             alert('Please select level');
@@ -48,56 +52,168 @@ $(document).ready(function () {
     //radio color on click
 
 
-    $(".radio-zero").click(function () {
-        $("#il-info-zero").css("color", "#4169E1");
-        $("#il-info-one").css("color", "black");
-        $("#il-info-two").css("color", "black");
-        $("#il-info-three").css("color", "black");
-        $("#il-info-four").css("color", "black");
-        $("#il-info-five").css("color", "black");
+     $("#radio-zero-symp-1").click(function () {
+        $("#il-info-symp-one-0").css("color", "#4169E1");
+        $("#il-info-symp-one-1").css("color", "black");
+        $("#il-info-symp-one-2").css("color", "black");
+        $("#il-info-symp-one-3").css("color", "black");
+        $("#il-info-symp-one-4").css("color", "black");
     });
-    $(".radio-one").click(function () {
-        $("#il-info-zero").css("color", "black");
-        $("#il-info-one").css("color", "#32CD32");
-        $("#il-info-two").css("color", "black");
-        $("#il-info-three").css("color", "black");
-        $("#il-info-four").css("color", "black");
-        $("#il-info-five").css("color", "black");
+    $("#radio-one-symp-1").click(function () {
+        $("#il-info-symp-one-0").css("color", "black");
+        $("#il-info-symp-one-1").css("color", "#32CD32");
+        $("#il-info-symp-one-2").css("color", "black");
+        $("#il-info-symp-one-3").css("color", "black");
+        $("#il-info-symp-one-4").css("color", "black");
     });
-    $(".radio-two").click(function () {
-        $("#il-info-zero").css("color", "black");
-        $("#il-info-one").css("color", "black");
-        $("#il-info-two").css("color", "#ffe066");
-        $("#il-info-three").css("color", "black");
-        $("#il-info-four").css("color", "black");
-        $("#il-info-five").css("color", "black");
+    $("#radio-two-symp-1").click(function () {
+        $("#il-info-symp-one-0").css("color", "black");
+        $("#il-info-symp-one-1").css("color", "black");
+        $("#il-info-symp-one-2").css("color", "#ffe066");
+        $("#il-info-symp-one-3").css("color", "black");
+        $("#il-info-symp-one-4").css("color", "black");
+
     });
-    $(".radio-three").click(function () {
-        $("#il-info-zero").css("color", "#black");
-        $("#il-info-one").css("color", "black");
-        $("#il-info-two").css("color", "black");
-        $("#il-info-three").css("color", "#ffa500");
-        $("#il-info-four").css("color", "black");
-        $("#il-info-five").css("color", "black");
+    $("#radio-three-symp-1").click(function () {
+        $("#il-info-symp-one-0").css("color", "black");
+        $("#il-info-symp-one-1").css("color", "black");
+        $("#il-info-symp-one-2").css("color", "black");
+        $("#il-info-symp-one-3").css("color", "#ffa500");
+        $("#il-info-symp-one-4").css("color", "black");
+
     });
-    $(".radio-four").click(function () {
-        $("#il-info-zero").css("color", "black");
-        $("#il-info-one").css("color", "black");
-        $("#il-info-two").css("color", "black");
-        $("#il-info-three").css("color", "black");
-        $("#il-info-four").css("color", "#8B4513");
-        $("#il-info-five").css("color", "black");
-    });
-    $(".radio-five").click(function () {
-        $("#il-info-zero").css("color", "black");
-        $("#il-info-one").css("color", "black");
-        $("#il-info-two").css("color", "black");
-        $("#il-info-three").css("color", "black");
-        $("#il-info-four").css("color", "black");
-        $("#il-info-five").css("color", "#ff0000");
+    $("#radio-four-symp-1").click(function () {
+        $("#il-info-symp-one-0").css("color", "black");
+        $("#il-info-symp-one-1").css("color", "black");
+        $("#il-info-symp-one-2").css("color", "black");
+        $("#il-info-symp-one-3").css("color", "black");
+        $("#il-info-symp-one-4").css("color", "#8B4513");
+
     });
 
 
+   //-----JS for report symptom two-------------
 
 
+    $(".symp-two").click(function () {
+        $(".section-symp-1,.section-symp-3").hide();
+         $(".section-symp-2").show();
+    });
+
+    //check if user chaked level
+    $('#save-symp-two').click(function () {
+        if (!$("input[name='symptom-2']:checked").val()) {
+            alert('Please select level');
+        } else {
+            $(".symp-two").css("background", "#d6f5d6");
+            $("#checked-symp-two").show();
+        }
+
+    });
+
+    //radio color on click
+
+
+     $("#radio-zero-symp-2").click(function () {
+        $("#il-info-symp-two-0").css("color", "#4169E1");
+        $("#il-info-symp-two-1").css("color", "black");
+        $("#il-info-symp-two-2").css("color", "black");
+        $("#il-info-symp-two-3").css("color", "black");
+        $("#il-info-symp-two-4").css("color", "black");
+    });
+    $("#radio-one-symp-2").click(function () {
+        $("#il-info-symp-two-0").css("color", "black");
+        $("#il-info-symp-two-1").css("color", "#32CD32");
+        $("#il-info-symp-two-2").css("color", "black");
+        $("#il-info-symp-two-3").css("color", "black");
+        $("#il-info-symp-two-4").css("color", "black");
+    });
+    $("#radio-two-symp-2").click(function () {
+        $("#il-info-symp-two-0").css("color", "black");
+        $("#il-info-symp-two-1").css("color", "black");
+        $("#il-info-symp-two-2").css("color", "#ffe066");
+        $("#il-info-symp-two-3").css("color", "black");
+        $("#il-info-symp-two-4").css("color", "black");
+
+    });
+    $("#radio-three-symp-2").click(function () {
+        $("#il-info-symp-two-0").css("color", "black");
+        $("#il-info-symp-two-1").css("color", "black");
+        $("#il-info-symp-two-2").css("color", "black");
+        $("#il-info-symp-two-3").css("color", "#ffa500");
+        $("#il-info-symp-two-4").css("color", "black");
+
+    });
+    $("#radio-four-symp-2").click(function () {
+        $("#il-info-symp-two-0").css("color", "black");
+        $("#il-info-symp-two-1").css("color", "black");
+        $("#il-info-symp-two-2").css("color", "black");
+        $("#il-info-symp-two-3").css("color", "black");
+        $("#il-info-symp-two-4").css("color", "#8B4513");
+
+    });
+
+//-----JS for report symptom three-------------
+
+
+    $(".symp-three").click(function () {
+        $(".section-symp-1,.section-symp-2").hide();
+         $(".section-symp-3").show();
+    });
+
+    //check if user chaked level
+    $('#save-symp-three').click(function () {
+        if (!$("input[name='symptom-3']:checked").val()) {
+            alert('Please select level');
+        } else {
+            $(".symp-three").css("background", "#d6f5d6");
+            $("#checked-symp-three").show();
+        }
+
+    });
+
+    //radio color on click
+
+
+     $("#radio-zero-symp-3").click(function () {
+        $("#il-info-symp-three-0").css("color", "#4169E1");
+        $("#il-info-symp-three-1").css("color", "black");
+        $("#il-info-symp-three-2").css("color", "black");
+        $("#il-info-symp-three-3").css("color", "black");
+        $("#il-info-symp-three-4").css("color", "black");
+    });
+    $("#radio-one-symp-3").click(function () {
+        $("#il-info-symp-three-0").css("color", "black");
+        $("#il-info-symp-three-1").css("color", "#32CD32");
+        $("#il-info-symp-three-2").css("color", "black");
+        $("#il-info-symp-three-3").css("color", "black");
+        $("#il-info-symp-three-4").css("color", "black");
+    });
+    $("#radio-two-symp-3").click(function () {
+        $("#il-info-symp-three-0").css("color", "black");
+        $("#il-info-symp-three-1").css("color", "black");
+        $("#il-info-symp-three-2").css("color", "#ffe066");
+        $("#il-info-symp-three-3").css("color", "black");
+        $("#il-info-symp-three-4").css("color", "black");
+
+    });
+    $("#radio-three-symp-3").click(function () {
+        $("#il-info-symp-three-0").css("color", "black");
+        $("#il-info-symp-three-1").css("color", "black");
+        $("#il-info-symp-three-2").css("color", "black");
+        $("#il-info-symp-three-3").css("color", "#ffa500");
+        $("#il-info-symp-three-4").css("color", "black");
+
+    });
+    $("#radio-four-symp-3").click(function () {
+        $("#il-info-symp-three-0").css("color", "black");
+        $("#il-info-symp-three-1").css("color", "black");
+        $("#il-info-symp-three-2").css("color", "black");
+        $("#il-info-symp-three-3").css("color", "black");
+        $("#il-info-symp-three-4").css("color", "#8B4513");
+
+    });
+
+
+   
 });
